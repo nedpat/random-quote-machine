7f0698ef8cdac69b1e28c8f5d8f51fc05dbdc77d
nedpat <patmarc@ywzi.com>
2020-11-22 20:37:25 +0200

    noreferrer noopener on links with target _blank

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import axios from "axios";
-import Quote from "./components/Quote";
-import Button from "./components/Button";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
-import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
-import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react';
+import axios from 'axios';
+import Quote from './components/Quote';
+import Button from './components/Button';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import "./scss/App.scss";
+import './scss/App.scss';
 
 library.add(faTwitterSquare, faGithubSquare, faQuoteLeft);
 
 export default class App extends React.Component {
   state = {
-    quote: "",
-    author: "",
-    search: "",
-    fade: "false",
-    urlImage: "",
-    color: "",
+    quote: '',
+    author: '',
+    search: '',
+    fade: 'false',
+    urlImage: '',
+    color: '',
   };
 
   componentDidMount() {
@@ -37,8 +37,8 @@ export default class App extends React.Component {
 
   fetchRandomQuote = () => {
     axios
-      .get("https://type.fit/api/quotes")
-      .then((response) => {
+      .get('https://type.fit/api/quotes')
+      .then(response => {
         let randomNumber = this.getRandomNumber(1643);
         const { text, author } = response.data[randomNumber];
         this.setState({
@@ -47,7 +47,7 @@ export default class App extends React.Component {
           fade: true,
         });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
   };
@@ -60,7 +60,7 @@ export default class App extends React.Component {
   };
 
   getHexColor = () => {
-    let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
     this.setState({ color: color });
   };
@@ -69,33 +69,33 @@ export default class App extends React.Component {
     const bodyStyle = document.body.style;
 
     bodyStyle.background = `url(${this.state.urlImage}) no-repeat center`;
-    bodyStyle.backgroundSize = "cover";
+    bodyStyle.backgroundSize = 'cover';
     bodyStyle.backgroundColor = this.state.color;
   };
 
   render() {
-    const quoteBtnText = "New quote";
+    const quoteBtnText = 'New quote';
     const twitterUrl = `https://twitter.com/intent/tweet?text=${this.state.quote} ${this.state.author}`;
 
     return (
-      <div className="app d-flex min-vh-100 align-items-center justify-content-center container">
+      <div className='app d-flex min-vh-100 align-items-center justify-content-center container'>
         <section
-          id="quote-box"
-          className="text-center text-lg-left p-5 bg-white rounded"
+          id='quote-box'
+          className='text-center text-lg-left p-5 bg-white rounded'
         >
-          <div className="row">
-            <div className="col-md-12 mb-4 mb-md-0">
+          <div className='row'>
+            <div className='col-md-12 mb-4 mb-md-0'>
               <div>
                 <a
-                  href="https://github.com/nedpat/random-quote-machine"
-                  target="_blank"
-                  rel="noreferrer"
-                  id="github-link"
+                  href='https://github.com/nedpat/random-quote-machine'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                  id='github-link'
                 >
                   <FontAwesomeIcon
-                    className="float-right icon"
+                    className='float-right icon'
                     icon={faGithubSquare}
-                    size="3x"
+                    size='3x'
                     color={this.state.color}
                   />
                 </a>
@@ -103,15 +103,15 @@ export default class App extends React.Component {
               <div>
                 <a
                   href={twitterUrl}
-                  target="_blank"
-                  rel="noreferrer"
-                  id="tweet-quote"
-                  className="twitter-share-button icon"
+                  target='_blank'
+                  rel='noreferrer noopener'
+                  id='tweet-quote'
+                  className='twitter-share-button icon'
                 >
                   <FontAwesomeIcon
-                    className="float-right icon"
+                    className='float-right icon'
                     icon={faTwitterSquare}
-                    size="3x"
+                    size='3x'
                     color={this.state.color}
                   />
                 </a>
@@ -123,7 +123,7 @@ export default class App extends React.Component {
                 fadeState={() => this.setState({ fade: false })}
                 color={this.state.color}
               />
-              <div className="float-left">
+              <div className='float-left'>
                 <Button
                   name={quoteBtnText}
                   search={this.fetchRandomQuote}
